a6e77cd0009af30f22b42a613d709d9bf3b6e717
Sadeem Sajid <sadeemsajid.25@gmail.com>
2024-10-14 22:01:57 +0500

    feat: enc & dec without translation

--- algorithms/src/main.rs
+++ algorithms/src/main.rs
@@ -1,37 +1,18 @@
 mod regev;
-use std::fmt::format;
-
-use ascii_converter::*;
-use ndarray::Array2;
+use ndarray::{Array1, Array2};
 
 fn main() {
-    // let params: regev::SecurityParameters = regev::setup();
+    let preamble: Array2<i32>;
+    let scalars: Array1<i32>;
 
-    // let pub_key: regev::PublicKey;
-    // let priv_key: regev::PrivateKey;
+    let pub_key: regev::PublicKey;
+    let priv_key: regev::PrivateKey;
 
-    // (pub_key, priv_key) = regev::key_gen(params);
+    let params: regev::SecurityParameters = regev::setup();
 
-    let og_vector: Vec<u32> = string_to_binary("I love lattices").unwrap();
+    (pub_key, priv_key) = regev::key_gen(&params);
 
-    let mut bit_stream: Vec<u32> = vec![];
+    (preamble, scalars) = regev::encrypt(String::from("I love lattices"), &pub_key, &params);
 
-    for byte in og_vector {
-        let mut byte_string: String = byte.to_string();
-
-        if byte_string.len() == 7 {
-            byte_string = String::from("0") + &byte_string;
-        } else if byte_string.len() == 6 {
-            byte_string = String::from("00") + &byte_string;
-        }
-
-        for bit in byte_string.chars() {
-            bit_stream.push(bit.to_digit(2).unwrap() as u32);
-        }
-    }
-
-    println!(
-        "{:?}",
-        Array2::from_shape_vec((1, bit_stream.len() as usize), bit_stream).unwrap()
-    );
+    let result = regev::decrypt(preamble, scalars, &priv_key, &params);
 }
--- algorithms/src/regev.rs
+++ algorithms/src/regev.rs
@@ -1,4 +1,5 @@
-use ndarray::Array2;
+use ascii_converter::*;
+use ndarray::{Array1, Array2};
 use rand::Rng;
 use rand_distr::{Distribution, Normal};
 
@@ -11,14 +12,14 @@ pub struct SecurityParameters {
 
 pub struct PublicKey {
     pub matrix: Array2<i32>,
-    pub public_vector: Array2<i32>,
+    pub public_vector: Array1<i32>,
 }
 
 pub struct PrivateKey {
-    pub secret_vector: Array2<i32>,
+    pub secret_vector: Array1<i32>,
 }
 
-fn __gen_random_matrix__(rows: i32, cols: i32, modulo: i32) -> Array2<i32> {
+fn __gen_random_array2__(rows: i32, cols: i32, modulo: i32) -> Array2<i32> {
     let mut matrix = Array2::<i32>::zeros((rows as usize, cols as usize));
 
     let mut rng = rand::thread_rng();
@@ -29,8 +30,45 @@ fn __gen_random_matrix__(rows: i32, cols: i32, modulo: i32) -> Array2<i32> {
     return matrix;
 }
 
-fn __error__(mean: f64, std_dev: f64, length: i32) -> Array2<i32> {
-    let mut matrix = Array2::<i32>::zeros((1, length as usize));
+fn __gen_random_array1__(size: i32, modulo: i32) -> Array1<i32> {
+    let mut matrix = Array1::<i32>::zeros(size as usize);
+
+    let mut rng = rand::thread_rng();
+    for elem in matrix.iter_mut() {
+        *elem = rng.gen_range(0..modulo);
+    }
+
+    return matrix;
+}
+
+pub fn __str_to_bit__(text: String) -> Array1<i32> {
+    let base_vector: Vec<u32> = string_to_binary(&text).unwrap();
+    let mut bit_stream: Vec<u32> = vec![];
+
+    for byte in base_vector {
+        let mut byte_string: String = byte.to_string();
+
+        if byte_string.len() == 7 {
+            byte_string = String::from("0") + &byte_string;
+        } else if byte_string.len() == 6 {
+            byte_string = String::from("00") + &byte_string;
+        }
+
+        for bit in byte_string.chars() {
+            bit_stream.push(bit.to_digit(2).unwrap() as u32);
+        }
+    }
+
+    return Array1::from(
+        bit_stream
+            .into_iter()
+            .map(|x: u32| x as i32)
+            .collect::<Vec<i32>>(),
+    );
+}
+
+fn __error__(mean: f64, std_dev: f64, length: i32) -> Array1<i32> {
+    let mut matrix = Array1::<i32>::zeros(length as usize);
     let normal: Normal<f64> = Normal::new(mean, std_dev).unwrap();
 
     for elem in matrix.iter_mut() {
@@ -53,21 +91,21 @@ pub fn setup() -> SecurityParameters {
     return params;
 }
 
-pub fn modify_params(_dimensions: i32, _rank: i32, _modulo: i32) -> SecurityParameters {
-    return SecurityParameters {
-        dimensions: _dimensions,
-        rank: _rank,
-        modulo: _modulo,
-    };
-}
+// pub fn modify_params(_dimensions: i32, _rank: i32, _modulo: i32) -> SecurityParameters {
+//     return SecurityParameters {
+//         dimensions: _dimensions,
+//         rank: _rank,
+//         modulo: _modulo,
+//     };
+// }
 
 // Call: 2
-pub fn key_gen(params: SecurityParameters) -> (PublicKey, PrivateKey) {
-    let a = __gen_random_matrix__(params.dimensions, params.rank, params.modulo);
+pub fn key_gen(params: &SecurityParameters) -> (PublicKey, PrivateKey) {
+    let a = __gen_random_array2__(params.dimensions, params.rank, params.modulo);
 
-    let s = __gen_random_matrix__(1, params.dimensions, params.modulo);
+    let s = __gen_random_array1__(params.dimensions, params.modulo);
 
-    let b = s.dot(&a) + __error__(0.0, 1.0, params.rank);
+    let b = s.dot(&a) + __error__(0.0, 0.3, params.rank);
 
     // Use this to show error
     // println!("Error: {:?}", &b - s.dot(&a));
@@ -82,6 +120,43 @@ pub fn key_gen(params: SecurityParameters) -> (PublicKey, PrivateKey) {
     );
 }
 
-pub fn encrypt(plain_text: String, public_key: PublicKey, params: SecurityParameters) {}
+pub fn encrypt(
+    plain_text: String,
+    public_key: &PublicKey,
+    params: &SecurityParameters,
+) -> (Array2<i32>, Array1<i32>) {
+    let bit_stream = __str_to_bit__(plain_text);
 
-// pub fn decrypt() {}
+    // random vector x
+    let x = __gen_random_array2__(params.rank, bit_stream.len() as i32, 2);
+
+    let preamble = public_key.matrix.dot(&x).mapv(|x: i32| x % params.modulo);
+
+    let scalars = (public_key.public_vector.dot(&x) + ((bit_stream * params.modulo) / 2))
+        .mapv(|x: i32| x % params.modulo);
+
+    return (preamble, scalars);
+}
+
+pub fn decrypt(
+    preabmle: Array2<i32>,
+    scalars: Array1<i32>,
+    private_key: &PrivateKey,
+    params: &SecurityParameters,
+) -> Array1<i32> {
+    let mut result =
+        (scalars - private_key.secret_vector.dot(&preabmle)).mapv(|x: i32| x.abs() % params.modulo);
+
+    // Use this to print result
+    println!("Result: {:?}", result);
+
+    for elem in result.iter_mut() {
+        if *elem <= params.modulo / 4 {
+            *elem = 0;
+        } else {
+            *elem = 1;
+        }
+    }
+
+    return result;
+}
