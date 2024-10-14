431bd8552c5f33b924e7d997540df9ce3222063b
Sadeem Sajid <sadeemsajid.25@gmail.com>
2024-10-14 22:22:05 +0500

    fix: result to string

--- algorithms/src/main.rs
+++ algorithms/src/main.rs
@@ -1,28 +1,6 @@
 mod regev;
 use ndarray::{Array1, Array2};
 
-fn bits_to_string(bits: Array1<i32>) -> String {
-    // Ensure the bit array length is divisible by 8.
-    assert!(
-        bits.len() % 8 == 0,
-        "Bit array length must be divisible by 8."
-    );
-
-    // Convert the bits into bytes.
-    let bytes: Vec<u8> = bits
-        .exact_chunks(8) // Split into 8-bit chunks.
-        .into_iter()
-        .map(|chunk| {
-            chunk.iter().enumerate().fold(0, |acc, (i, &bit)| {
-                acc | ((bit as u8) << (7 - i)) // Convert chunk to a byte (big-endian).
-            })
-        })
-        .collect();
-
-    // Convert the bytes to a UTF-8 string.
-    String::from_utf8(bytes).expect("Invalid UTF-8 sequence")
-}
-
 fn main() {
     let preamble: Array2<i32>;
     let scalars: Array1<i32>;
@@ -42,5 +20,5 @@ fn main() {
 
     let result = regev::decrypt(preamble, scalars, &priv_key, &params);
 
-    println!("Decrypted Text: {:?}", bits_to_string(result));
+    println!("Decrypted Text: {:?}", result);
 }
--- algorithms/src/regev.rs
+++ algorithms/src/regev.rs
@@ -67,6 +67,28 @@ pub fn __str_to_bit__(text: String) -> Array1<i32> {
     );
 }
 
+fn __bits_to_string__(bits: Array1<i32>) -> String {
+    // Ensure the bit array length is divisible by 8.
+    assert!(
+        bits.len() % 8 == 0,
+        "Bit array length must be divisible by 8."
+    );
+
+    // Convert the bits into bytes.
+    let bytes: Vec<u8> = bits
+        .exact_chunks(8) // Split into 8-bit chunks.
+        .into_iter()
+        .map(|chunk| {
+            chunk.iter().enumerate().fold(0, |acc, (i, &bit)| {
+                acc | ((bit as u8) << (7 - i)) // Convert chunk to a byte (big-endian).
+            })
+        })
+        .collect();
+
+    // Convert the bytes to a UTF-8 string.
+    String::from_utf8(bytes).expect("Invalid UTF-8 sequence")
+}
+
 fn __error__(mean: f64, std_dev: f64, length: i32) -> Array1<i32> {
     let mut matrix = Array1::<i32>::zeros(length as usize);
     let normal: Normal<f64> = Normal::new(mean, std_dev).unwrap();
@@ -143,7 +165,7 @@ pub fn decrypt(
     scalars: Array1<i32>,
     private_key: &PrivateKey,
     params: &SecurityParameters,
-) -> Array1<i32> {
+) -> String {
     let mut result =
         (scalars - private_key.secret_vector.dot(&preabmle)).mapv(|x: i32| x.abs() % params.modulo);
 
@@ -158,5 +180,5 @@ pub fn decrypt(
         }
     }
 
-    return result;
+    return __bits_to_string__(result);
 }
